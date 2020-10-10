aa7bdff9cb7ae05dad152549a0cd517bd7de5008
AVATAR ID <68086971+romli2020@users.noreply.github.com>
2020-10-10 12:04:26 +0700

    Add files via upload

---
+++ IF.sh
@@ -0,0 +1,309 @@
+#usr/bin/bash
+
+clear
+sleep 1
+echo "       ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+sleep 1
+echo "       █            MENU TOOLS                 █"
+sleep 1
+echo "       █        TOOLS BY AVATAR ID             █"
+sleep 1
+echo "       █       GUNAKAN DENGAN BIJAK            █"
+sleep 1
+echo "       █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  NO  ║ ║            MENU TOOLS             ║ ║     STATUS     ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  01  ║ ║  [   Hack Fb Target no Login  ]   ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  02  ║ ║  [  BRUTE FORCE FB NO LOGIN  ]    ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╦ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  03  ║ ║    [  PHISING   FACEBOOK  ]       ║ ║      ON        ║"
+echo "╚══════╩ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  04  ║ ║      [  DARK FB PREMIUM  ]        ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  05  ║ ║     [  HACK FACEBOOK LOGIN ]      ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  06  ║ ║ [ TOOLS DARK FB NO LOGIN SENSEI ] ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  07  ║ ║    [ TOOLS KHUSUS FACEBOOK ]      ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  08  ║ ║      [  REPORT FACEBOK ]          ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  09  ║ ║       [ HACK FACEBOOK ]           ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  10  ║ ║   [   CURI INFORMASI TEMAN  ]     ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  11  ║ ║     [  AUTO LIKE FACEBOOK ]       ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  12  ║ ║ [ TOOLS DARK Fb NO LOGIN RIZKY  ] ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  13  ║ ║      [ PROFIL FB GUARD ]          ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  14  ║ ║ [JUMLAH PASIEN COVID 19 DI INDO ] ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  15  ║ ║   [ DOWLOAD VIDEO FACEBOOK ]      ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  16  ║ ║  [ TOMBOL TAMBAHAN TERMUX  ]      ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  17  ║ ║     [   BRUTAL SPAM SMS  ]        ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  18  ║ ║     [  TOOLS DDOS ATTACK  ]       ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  19  ║ ║  [ TROJAN VIA  NOMOR TARGET  ]    ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  20  ║ ║   [  VIRTEX WHATSHAP GANAS  ]     ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  21  ║ ║       [ INSTALL  BAHAN  ]         ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  22  ║ ║  [ SUBSCROBE CANNEL AVATAR ID ]   ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+echo "╔══════╗ ╔═══════════════════════════════════╗ ╔════════════════╗"
+echo "║  00  ║ ║         [ EXIT TOOLS ]            ║ ║      ON        ║"
+echo "╚══════╝ ╚═══════════════════════════════════╝ ╚════════════════╝"
+sleep 1
+read -p " pilih : >>  " pil :
+#batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/FR13ND8/BruteFb
+ls
+cd BruteFb
+ls
+python2 brute.py
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/pirmansx/mbf
+ls
+cd mbf
+ls
+python2 MBF.py
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+ls
+cd SocialSploit
+ls
+bash install.sh
+bash Sploit
+fi
+#batas gan
+if [ $pil = 4 ]
+then
+git clone https://github.com/TheMagizz/DarkPremium
+ls
+cd DarkPremium
+python2 DarkFB.py
+fi
+#batas gan
+if [ $pil = 5 ]
+then
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+ls
+cd hack-facebook-teman
+python2 hack-fb.py
+fi
+#batas gan
+if [ $pil = 6 ]
+then
+git clone https://github.com/BOT-033/Sensei
+ls
+cd Sensei
+python2 main.py
+fi
+#batas gan
+if [ $pil = 7 ]
+then
+git clone https://github.com/DarkCurut08/DarkCurut08
+ls
+cd DarkCurut08
+sh Moreno77.sh
+fi
+#batas gan
+if [ $pil = 8 ]
+then
+git clone https://github.com/IlayTamvan/Report
+ls
+cd Report
+unzip Report.zip
+
+python2 Report.py
+fi
+#batas gan
+if [ $pil = 9 ]
+then
+git clone https://github.com/AMVengeance/FB-React
+ls
+cd FB-React
+bash start
+fi
+#batas gan
+if [ $pil = 10 ]
+then
+git clone https://github.com/FR13ND8/InfoFB
+ls
+cd InfoFB
+ls
+pip2 install -r requirements.txt
+python2 info.py
+fi
+#batas gan
+if [ $pil = 11 ]
+then
+git clone https://github.com/FR13ND8/autolike
+ls
+cd autolike
+php autolike.php
+fi
+#batas gan
+if [ $pil = 12 ]
+then
+git clone https://github.com/RIZKY4/pro
+ls
+cd pro
+python2 pro.py
+fi
+#batas gan
+if [ $pil = 13 ]
+then
+git clone https://github.com/storiku/ppguard 
+ls
+cd ppguard
+php fb.php
+fi
+#batas gan
+if [ $pil = 14 ]
+then
+git clone https://github.com/Pandas-ID/corona
+ls
+cd corona
+python corona.py
+fi
+#batas gan
+if [ $pil = 15 ]
+then
+git clone https://github.com/Pandas-ID/fb-video-downloader
+ls
+cd fb-video-downloader
+python downloader.py
+fi
+#batas gan
+if [ $pil = 16 ]
+then
+git clone https://github.com/Hackers20/Tombol-Termux77
+ls
+cd Tombol-Termux77
+python2 tombol.py
+fi
+# batas gan
+if [ $pil = 17 ]
+then
+git clone https://github.com/Zidan-ID17/Brutal-Spam-SCW
+ls
+cd Brutal-Spam-SCW
+python2 umbrella.py
+fi
+# batas gan
+if [ $pil = 18 ]
+then
+git clone https://github.com/Romli90000/Tools-Ddos
+cd Tools-Ddos
+bash attack.sh
+fi
+if [ $pil = 19 ]
+then
+git clone https://github.com/Romli90000/Tools-Trojan
+cd Tools-Trojan
+bash trojan.sh
+fi
+# batas gan
+if [ $pil = 20 ]
+then
+git clone https://github.com/IntiTutorial/VIRTEX
+cd VIRTEX
+chmod +x virtex.sh
+sh virtex.sh
+fi
+# batas gan
+if [ $pil = 21 ]
+then
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pkg install wget
+pkg install bash
+pkg install cowsay
+pkg install php
+pkg install clang
+pkg install figlet
+pip install lolcat
+pip install requests
+pip install mechanize
+pip install bs4
+fi
+# batas gan
+if [ $pil = 22 ]
+then
+echo "          [ LINK YT GW GAN DI SUBSCRIBE YA ] " | lolcat
+echo
+echo "  [  https://m.youtube.com/channel/UCzqK0ZO3Iyjr8Q4JozBBiuA  ]" | lolcat
+echo
+echo "               [  THANKS FOR WATCHING  ]" | lolcat
+fi
+if [ $pil = 00 ]
+then
+echo '    [   THANKS FOR WATCHING  ]' | lolcat
+echo '   [ SUBSCRIBE CANNEL AVATAR ID ]' | lolcat
+echo '             ✋✋✋✋'
+exit
+fi
